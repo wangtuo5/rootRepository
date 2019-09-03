ba4e7189107df8ccd989a52205a3f3699ff72c13
wangtuo5 <695167546@qq.com>
2019-09-03 22:24:13 +0800

    git项目提交

---
+++ netctoss1/src/demo44.java
@@ -0,0 +1,17 @@
+public class demo44 {
+    public static void main(String[] args) {
+        int c =11;
+        int d =11;
+        int e =11;
+        int f =11;
+        int ff =11;
+        int g =11;
+        int h =11;
+        int y =11;
+        int hh =11;
+        int yh =11;
+        int yh2 =11;
+        int kk = 22;
+        int aa = 33;
+    }
+}
